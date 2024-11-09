8df38d827a0602cf6b2314efdb960aa7f972f6ec
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-11-09 21:21:10 +0900

    Update .lua

--- .lua
+++ .lua
@@ -2,22 +2,27 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Roboj
 
 local Window = Library.CreateLib("Ozon script", "RJTheme3")
 
-local Tab = Window:NewTab("Im hate ozon WB better")
+local Tab = Window:NewTab("Main")
 
-local Section = Tab:NewSection("Main")
+local Section = Tab:NewSection("Im hate ozon WB better")
 
-Section:NewLabel("LabelText")
+Section:NewLabel("Make by a weirdman2112")
 
 Section:NewButton("Add npc", "Add npc to you", function()
     game:GetService("ReplicatedStorage").Events.NPCEvents.CreateNPC:FireServer()
 end)
 
--- Переключатель
+Section:NewButton("delete npc", "delete npc from you", function()
+    game:GetService("ReplicatedStorage").Events.NPCEvents.DeleteNPC:FireServer()
+end)
+
 Section:NewToggle("ToggleText", "ToggleInfo", function(state)
     if state then
-        print("Toggle On")
+        while wait() do
+	game:GetService("ReplicatedStorage").Events.CreateBoxEvent:FireServer()
+	end
     else
-        print("Toggle Off")
+        break
     end
 end)
 
