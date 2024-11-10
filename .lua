4b55e63e28095f0d2ceb3b3232524199c5fb6c6d
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-11-10 12:36:07 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,4 +1,3 @@
-
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
 local Window = Library.CreateLib("Ozon script", "RJTheme3")
