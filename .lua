ea49d7ab5640872a352e678a87aa45ce8fbfdaa1
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-11-10 13:24:08 +0900

    Update .lua

--- .lua
+++ .lua
@@ -4,13 +4,13 @@ ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ResetOnSpawn = false
  
 local Toggle = Instance.new("TextButton")
-Toggle.Name = "Toggle GUI"
+Toggle.Name = "Toggle"
 Toggle.Parent = ScreenGui
 Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
 Toggle.Size = UDim2.new(0, 90, 0, 38)
 Toggle.Font = Enum.Font.SourceSans
-Toggle.Text = "Ozon script"
+Toggle.Text = "Toggle"
 Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
 Toggle.TextSize = 28.000
 Toggle.Draggable = true
